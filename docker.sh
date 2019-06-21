b45783d4ac629ac2b8b85ad9f4f7f37768ea8103
Duncaen <mail@duncano.de>
2019-06-21 19:54:17 +0200

    fix

--- docker.sh
+++ docker.sh
@@ -2,8 +2,6 @@
 #
 # docker.sh
 
-[ "$XLINT" ] && exit 0
-
 /bin/echo -e "\x1b[32mPulling docker image for $BOOTSTRAP from $TAG...\x1b[0m"
 docker pull voidlinux/masterdir-$BOOTSTRAP:$TAG
 docker run -d \
--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 
-xbps-install -Su
-xbps-install -S strace
+xbps-install -Suy
+xbps-install -Sy strace
 strace xbps-fetch -dv http://ftp.debian.org/debian/pool/main/d/dpkg/dpkg_1.19.7.tar.xz
+strace xbps-fetch -dv https://people.torproject.org/~yawning/releases/obfs4proxy/obfs4proxy-0.0.11.tar.xz
